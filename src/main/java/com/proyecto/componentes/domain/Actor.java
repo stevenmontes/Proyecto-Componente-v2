322c49bba59bbd2af7831d6829f12354ea5410db
Daniela <dgarciaj@ucenfotec.ac.cr>
2019-07-27 11:30:14 -0600

    creacion de objeto actor y actorRepository

---
+++ src/main/java/com/proyecto/componentes/domain/Actor.java
@@ -0,0 +1,87 @@
+package com.proyecto.componentes.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TBL_ACTOR")
+public class Actor {
+	@Id
+	@Column(name = "ID")
+	private int id;
+	@Column(name = "NOMBRE")
+	private String nombre;
+	@Column(name = "DESCRIPCION")
+	private String descripcion;
+	@Column(name="ESTADO")
+	private boolean estado;
+	
+	public Actor(int id, String nombre, String descripcion, boolean estado) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.estado = estado;
+	}
+
+	public Actor() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+	@Override
+	public String toString() {
+		return "Actor [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", estado=" + estado + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
