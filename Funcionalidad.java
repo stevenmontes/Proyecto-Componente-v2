79149b1fcfc37cc0c7ba8ac87c949fa667e5e0b6
shannon1720 <39140859+shannon1720@users.noreply.github.com>
2019-07-27 19:37:20 -0600

    prueba3

---
+++ Funcionalidad.java
@@ -0,0 +1,101 @@
+package com.proyecto.componentes.domain;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TBL_FUNCIONALIDAD")
+public class Funcionalidad {
+	@Id
+	@Column(name = "CODIGO")
+	private String codigo;
+	@Column(name = "NOMBRE")
+	private String nombre;
+	@Column(name = "DESCRIPCION")
+	private String descripcion;
+	@Column(name="PRIORIDAD")
+	private int prioridad;
+	@ManyToOne
+	 @JoinColumn(name="ID_PROYECTO")
+	  private Proyecto proyecto;
+
+	@Column(name="ESTADO")
+	private boolean estado;
+	
+	public Funcionalidad(String codigo, String nombre, String descripcion, int prioridad, Proyecto proyecto) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;     
+		this.descripcion = descripcion;
+		this.prioridad = prioridad;
+		this.proyecto = proyecto;
+	}
+
+	public Funcionalidad() {
+		
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public int getPrioridad() {
+		return prioridad;
+	}
+
+	public void setPrioridad(int prioridad) {
+		this.prioridad = prioridad;
+	}
+
+	public Proyecto getProyecto() {
+		return proyecto;
+	}
+
+	public void setProyecto(Proyecto proyecto) {
+		this.proyecto = proyecto;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+	@Override
+	public String toString() {
+		return "Funcionalidad [codigo=" + codigo + ", nombre=" + nombre + ", descripcion=" + descripcion
+				+ ", prioridad=" + prioridad + ", proyecto=" + proyecto + ", estado=" + estado + "]";
+	}
+
+
+	
+	
+}
---
+++ Proyecto.java
@@ -0,0 +1,76 @@
+package com.proyecto.componentes.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TBL_PROYECTO")
+public class Proyecto {
+	@Id
+	@Column(name = "CODIGO")
+	private String codigo;
+	@Column(name = "NOMBRE")
+	private String nombre;
+	@Column(name = "DESCRIPCION")
+	private String descripcion;
+	@Column(name = "VERSION")
+	private String version;
+	@OneToMany(mappedBy = "proyecto")
+    private List<Funcionalidad> funcionalidades = new ArrayList<Funcionalidad>();
+	public Proyecto() {
+		super();
+	}
+
+	public Proyecto(String codigo, String nombre, String descripcion, String version) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.version = version;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	@Override
+	public String toString() {
+		return "Proyecto [codigo=" + codigo + ", nombre=" + nombre + ", descripcion=" + descripcion + ", version="
+				+ version + "]";
+	}
+
+}
