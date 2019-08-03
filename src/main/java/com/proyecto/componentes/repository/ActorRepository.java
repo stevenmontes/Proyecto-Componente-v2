f973dc2b8fd3a9e54a478db185dc35d36afdaf76
Steven Montes <cmontesa@ucenfotec.ac.cr>
2019-08-03 12:53:20 -0600

    Cleanup Code

--- src/main/java/com/proyecto/componentes/repository/ActorRepository.java
+++ src/main/java/com/proyecto/componentes/repository/ActorRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.proyecto.componentes.domain.Actor;
 
-
 public interface ActorRepository extends JpaRepository<Actor, Integer> {
 	Actor findById(int id);
 }
--- src/main/java/com/proyecto/componentes/repository/FuncionalidadRepository.java
+++ src/main/java/com/proyecto/componentes/repository/FuncionalidadRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.proyecto.componentes.domain.Funcionalidad;
 
-
 public interface FuncionalidadRepository extends JpaRepository<Funcionalidad, String> {
 	Funcionalidad findByCodigo(String codigo);
 
